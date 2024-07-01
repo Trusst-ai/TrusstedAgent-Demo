9385117d7ddf45a967a9c6b868457bd9ace25b4a
Camilo Rivera <camilo@trusst.ai>
2024-07-01 13:24:36 +1000

    Rename synthesizer package

--- text_to_speech/fastspeech_synthesizer_app.py
+++ text_to_speech/fastspeech_synthesizer_app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, send_file, make_response
 import os
 import uuid
 import io
+import boto3
 
 import malaya_speech
 import numpy as np
@@ -15,6 +16,9 @@ app = Flask(__name__)
 universal_melgan = malaya_speech.vocoder.melgan(model = 'universal-384')
 male_tacotron2 = malaya_speech.tts.tacotron2(model = 'osman')
 
+bucketName = 'trussted-agent-text-to-speech-bucket'
+audioFileName = 'file.wav'
+
 @app.route('/synthesize', methods=['POST'])
 def synthesize():
     data = request.get_json()
@@ -26,10 +30,18 @@ def synthesize():
     r_male_tacotron2 = male_tacotron2.predict(text)
     data = universal_melgan(r_male_tacotron2['universal-output'])
     
-    # TODO: Send file over to S3 bucket
-    sf.write('file.wav', data, 22050)
+    # Write to local system (we'll buffer it into S3)
+    write_to_local_fs(data)
+    write_to_s3(data)
 
     return make_response(jsonify({'success':True}), 200)
+
+def write_to_local_fs(data):
+    sf.write(audioFileName, data, 22050)
+
+def write_to_s3(data):
+    s3 = boto3.client("s3")
+    s3.Object(bucketName, audioFileName).put(Body=open(audioFileName, 'rb'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
