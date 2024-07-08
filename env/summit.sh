59a128ee943a50f17876c338f9cfa800c1a606b5
David Pinto <david@trusst.ai>
2024-07-08 17:02:19 +1000

    Updated to deploy to summit account

---
+++ env/summit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Change to test, uat, prod etc
+export stage=summit
+
+# Should not need to change
+export service=trusstedagent
+
+# Target AWS deployment region
+export region=ap-southeast-2
+
+# Bedrock region
+export bedrockRegion=ap-southeast-2
+
+export AWS_REGION=$region
+
+export AWS_PROFILE=summit
+
+if [ -z "$AWS_PROFILE" ]; then
+  echo "Please set AWS_PROFILE=<<your-aws-profile>> before continuing..."
+  exit 1
+fi
+
+echo "Using AWS_PROFILE=${AWS_PROFILE}"
+
+# AWS account number
+export accountNumber=$(aws sts get-caller-identity --query Account --output text)
+
+# Whisper V3
+export whisperEndPoint=lissten-endpoint
+
+# S3 bucket to upload deployment assets to
+export deploymentBucket="${stage}-${service}-deployment-${accountNumber}"
+
+echo "Exported $stage"
